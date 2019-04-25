97648b275d1f3d26f999a9bd6f8e48ec7d4200bc
Leonardo Javier Russo <lrusso@Mac-Mini-2.local>
2019-04-25 13:06:53 -0300

    -
    -

--- Andruino Bluetooth/app/src/main/java/ar/com/lrusso/andruinobluetooth/Main.java
+++ Andruino Bluetooth/app/src/main/java/ar/com/lrusso/andruinobluetooth/Main.java
@@ -430,22 +430,11 @@ public class Main extends Activity
 	
 	private void clickInAbout()
 		{
-		String anos = "";
-		String valor = getResources().getString(R.string.textAboutMessage);
-		int lastTwoDigits = Calendar.getInstance().get(Calendar.YEAR) % 100;
-		if (lastTwoDigits<=5)
-			{
-			anos = "2005";
-			}
-			else
-			{
-			anos ="2005 - 20" + String.valueOf(lastTwoDigits).trim();
-			}
-		
-		valor = valor.replace("ANOS", anos);
+		String value = getResources().getString(R.string.textAboutMessage);
+		value = value.replace("APPNAME",getResources().getString(R.string.app_name));
 		
 		TextView msg = new TextView(this);
-		msg.setText(Html.fromHtml(valor));
+		msg.setText(Html.fromHtml(value));
 		msg.setPadding(10, 20, 10, 25);
 		msg.setGravity(Gravity.CENTER);
 		float scaledDensity = getResources().getDisplayMetrics().scaledDensity;
